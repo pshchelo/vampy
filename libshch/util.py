19504a20a04595ecea6c260cbf1ed39c7dbd08db
pshchelo <>
2009-11-10 09:02:17 +0000

    +added weighted average function

--- libshch/util.py
+++ libshch/util.py
@@ -4,6 +4,7 @@ Various useful supporting functions
 
 import math
 from scipy import ndimage
+import numpy as np
 
 def split_to_int(line, dflt=None):
     mesg=None
@@ -36,3 +37,10 @@ def grid_size(N):
 
 def get_gradient(x, sigma):
     return ndimage.gaussian_gradient_magnitude(ndimage.sobel(x), sigma)
+    
+def weighted_average(x, sd):
+    '''Calculates weighted average and it's standard deviation (normal and corrected for under/over dispersion'''
+    avew = np.average(x, weights=sd)
+    avewvar= 1/np.sum(1/sd**2)
+    avewvarcorr=avewvar*np.sum((x-avew)**2/sd**2)/(len(x)-1)
+    return avew, np.sqrt(avewvar), np.sqrt(avewvarcorr)
