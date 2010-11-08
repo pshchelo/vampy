da4ccf32b7d8f9198450e1e4dabd9e3a1aa89022
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2010-11-08 16:49:00 +0100

    moved drawing of plots to separate method enabled opening the GeometryFrame as main frame, without input data

--- vampy/wxgui/geometry.py
+++ vampy/wxgui/geometry.py
@@ -17,10 +17,8 @@ from libshch.common import MEASURE, SAVETXT, OPENTXT
 from libshch import util, wxutil
 
 class GeometryFrame(wx.Frame):
-    def __init__(self, parent, id, argsdict):
+    def __init__(self, parent, id, argsdict=None):
         wx.Frame.__init__(self, parent, id, size = (1024,768), title = 'Vesicle Geometry')
-        
-        self.data = argsdict
         
         self.statusbar = wxutil.PlotStatusBar(self)
         self.SetStatusBar(self.statusbar)
@@ -32,38 +30,25 @@ class GeometryFrame(wx.Frame):
         panel = wx.Panel(self, -1)
         pansizer = wx.BoxSizer(wx.VERTICAL)
         self.figure = Figure(facecolor = wxutil.rgba_wx2mplt(panel.GetBackgroundColour()))
-        self.figure.suptitle('Pipette radius (px): %f +-%f'%tuple(self.data['piprad']))
         self.canvas = FigureCanvas(panel, -1, self.figure)
         self.canvas.mpl_connect('motion_notify_event', self.statusbar.SetPosition)
         pansizer.Add(self.canvas, 1, wx.GROW)
-        titles = dict(aspl='Aspirated Length',
-                      vesrad='Vesicle Radius',
-                      area='Area',
-                      vesl='Outer Radius',
-                      volume='Volume',
-                      angle = 'Axis angle',
-                      metrics = 'metric')
-        toplot = ['aspl', 'vesl', 'metrics', 'area']
-        self.plots = {}
-        x = range(1, len(argsdict[toplot[0]][0])+1)
-        n,m = util.grid_size(len(toplot))
-        for index, item in enumerate(toplot):
-            y, y_err = self.data[item]
-            plottitle = titles[item]
-            plot = self.figure.add_subplot(n,m,index+1, title = plottitle)
-            plot.errorbar(x, y, y_err, fmt='bo-', label=plottitle)
-            self.plots[item] = plot
-#       
+        
         navtoolbar = NavigationToolbar2(self.canvas)
         navtoolbar.Realize()
         pansizer.Add(navtoolbar, 0, wx.GROW)
         
         panel.SetSizer(pansizer)
         panel.Fit()
-        title = '%s : %s - %s'%(parent.imagedate, parent.imagedir, self.GetTitle())
-        self.SetTitle(title)
         self.SetFrameIcons(MEASURE, (16,24,32))
-        self.canvas.draw()
+        
+        if argsdict:
+            self.data = argsdict
+            title = '%s : %s - %s'%(parent.imagedate, parent.imagedir, self.GetTitle())
+            self.SetTitle(title)
+        else:
+            self.data = {}
+        self.Draw()
     
     def SetFrameIcons(self, artid, sizes):
         ib = wx.IconBundle()
@@ -95,4 +80,37 @@ class GeometryFrame(wx.Frame):
         writer = output.DataWriter(self.data, title='Vesicle geometry')
         mesg = writer.write_file(datname)
         if mesg:
-            self.GetParent().OnError(mesg)
+            self.OnError(mesg)
+    
+    def OnError(self, msg):
+        """Display an error dialog
+        
+        @param msg: error message to display (type = string)
+        """
+        errDlg = wx.MessageDialog(self, msg, "Error!", wx.ICON_ERROR|wx.OK)
+        errDlg.ShowModal()
+        errDlg.Destroy()
+    
+    def Draw(self):
+        """Refresh the plot(s)"""
+        titles = dict(aspl='Aspirated Length',
+                      vesrad='Vesicle Radius',
+                      area='Area',
+                      vesl='Outer Radius',
+                      volume='Volume',
+                      angle = 'Axis angle',
+                      metrics = 'metric')
+        toplot = ['aspl', 'vesl', 'metrics', 'area']
+        self.plots = {}
+        x = range(1, len(self.data.get(toplot[0], [[],[]])[0])+1)
+        n,m = util.grid_size(len(toplot))
+        self.figure.clear()
+        for index, item in enumerate(toplot):
+            y, y_err = self.data.get(item, [[],[]])
+            plottitle = titles[item]
+            plot = self.figure.add_subplot(n,m,index+1, title = plottitle)
+            if x:
+                plot.errorbar(x, y, y_err, fmt='bo-', label=plottitle)
+#            self.plots[item] = plot
+        self.figure.suptitle('Pipette radius (px): %f +-%f'%tuple(self.data.get('piprad', (0,0))))
+        self.canvas.draw()
