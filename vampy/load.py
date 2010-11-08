8bb686a8f239edd96f447ff412bb9bbb2faacd09
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2010-11-08 15:46:40 +0100

    added reading of hand-measured gemetry files

--- vampy/load.py
+++ vampy/load.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 ### for loading images to numpy arrays with PIL
 from scipy import misc
+from vampy.common import PIX_ERR
 
 def read_grey_image(filename):
     '''read single greyscale image'''
@@ -63,19 +64,6 @@ def read_pressures_file(filename, stage):
     @param filename:
     @param stage:
     """
-#    presfile = open(filename, 'r')
-#    lines = presfile.readlines()
-#    presfile.close()
-#    pressures = []
-#    for line in lines:
-#        if line[0] != '#':
-#            elements = line.split()
-#            try:
-#                pressure = float(elements[stage])
-#            except(ValueError):
-#                mesg = 'Wrong pressure file format! (%s)'%line
-#                return None, mesg
-#            pressures.append(pressure)
     try:
         pressures = np.loadtxt(filename, unpack=1)
     except IOError, value:
@@ -127,3 +115,42 @@ def read_tensions(filename):
     tensiondata['dilation'] = data[1:3]
     tensiondata['tension'] = data[3:5]
     return tensiondata, None
+
+def read_geometry_simple(filename):
+    """Read hand-measured geometry file
+    
+    This must be a tab-separated file (with #-comments if necessary) with 4 columns:
+    pipette radius, 
+    position of aspirated vesicle part, 
+    position of pipette mouth, 
+    position of outside part of the vesicle.
+    
+    The distances are assumed to already corrected by the tilt (as ImageJ does it), 
+    so that the metrics is 1 and it's error is zero.
+    
+    The single errors are taken to be sqrt(2)*PIX_ERR for pipette radius, 
+    and PIX_ERR for everything else.
+    
+    Returns dictionary of geometry data as accepted by analysis routines 
+    and a message with reason of failure if any.
+    
+    """
+    try:
+        data = np.loadtxt(filename, unpack=True)
+    except IOError, value:
+        return None, value
+    except ValueError, value:
+        return None, value
+    piprad, asp, pip, ves = data
+    out={}
+    out['piprads'] = np.asarray((piprad, PIX_ERR*np.sqrt(2)*np.ones_like(piprad)))
+    out['asps'] = np.asarray((asp, PIX_ERR*np.ones_like(asp)))
+    out['pips'] = np.asarray((pip, PIX_ERR*np.ones_like(pip)))
+    out['vess'] = np.asarray((ves, PIX_ERR*np.ones_like(ves)))
+    out['metrics'] = np.asarray((np.ones_like(asp), np.zeros_like(asp)))
+    return out, None
+    
+    
+
+def read_geometry_full():
+    pass
